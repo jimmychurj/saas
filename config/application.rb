0ffb6455ec123a67767107642857373d690beda3
rjchu@uwaterloo.ca <ubuntu@ip-172-31-20-218.us-east-2.compute.internal>
2020-10-18 03:08:17 +0000

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,3 @@ module Saas
     # the framework and any gems in your application.
   end
 end
-class Application < Rails::Application
-  config.web_console.whitelisted_ips = '206.176.135.246'
-end
